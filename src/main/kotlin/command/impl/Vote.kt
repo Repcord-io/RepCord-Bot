6ab29cd7ed2bbf944b80e006f0863a3938226a7c
Tony <tonycos92@gmail.com>
2020-11-07 00:23:16 -0600

    Helper Class Reformat

--- src/main/kotlin/command/impl/Vote.kt
+++ src/main/kotlin/command/impl/Vote.kt
@@ -1,12 +1,11 @@
 package command.impl
 
-import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class Vote : ListenerAdapter() {
     override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
-        val embed = utils.helper.createEmbed("Vote")
+        val embed = utils.Helper.createEmbed("Vote")
         embed.addField(
             "Perks",
             "By voting for us you gain an a rep power boost of +2 for 12 hours!\nThough remember to vote again once the 12 hours have passed to retain your boost.",
--- src/main/kotlin/utils/Helper.kt
+++ src/main/kotlin/utils/Helper.kt
@@ -1,12 +1,14 @@
 package utils
 
 import net.dv8tion.jda.api.EmbedBuilder
+import java.awt.Color
 
-object helper {
+object Helper {
     fun createEmbed(title: String) : EmbedBuilder {
         val embed = EmbedBuilder()
         embed.setTitle(title)
         embed.setFooter("RepCord - https://repcord.io")
+        embed.setColor(Color.decode("#5262a6"))
         return embed
     }
 }
