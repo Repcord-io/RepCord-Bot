950c8f975e6ca7fffdf330333fc169de4736aabf
Tony <tonycos92@gmail.com>
2020-11-08 00:20:12 -0600

    Embed Helper Method

--- src/main/kotlin/utils/Helper.kt
+++ src/main/kotlin/utils/Helper.kt
@@ -23,4 +23,17 @@ object Helper {
     fun queueEmbed(event: GuildMessageReceivedEvent, embedBuilder: EmbedBuilder) {
         event.channel.sendMessage(embedBuilder.build()).queue();
     }
+
+    fun embed(event: GuildMessageReceivedEvent, title: String = "", send: Boolean = false, description: String = "") : EmbedBuilder {
+        val embed = EmbedBuilder()
+        embed.setTitle(title)
+        if (description.isNotBlank())
+            embed.setDescription(description)
+        embed.setFooter("RepCord - https://repcord.io")
+        embed.setColor(Color.decode("#5262a6"))
+        val messAction = event.channel.sendMessage(embed.build())
+        if (send)
+            messAction.queue()
+        return embed
+    }
 }
