f262503219abf859e9cbc6108de8f5850c80a555
Rotinx <39930952+Rotinx@users.noreply.github.com>
2020-11-08 15:30:05 +1300

    Added help command, command also runs with default prefix

--- src/main/kotlin/command/impl/Help.kt
+++ src/main/kotlin/command/impl/Help.kt
@@ -1,7 +1,11 @@
 package command.impl
 
+import command.Commands
+import database.impl.Guild
+import database.impl.Prefix
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import utils.Helper
 
 
 /*
@@ -12,6 +16,12 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 class Help : ListenerAdapter() {
 
     override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
-        println("Help Received")
+        val embed = Helper.createEmbed("Help")
+        embed.setDescription("RepCord is a cross-server reputation bot.\nKnow who your dealing with, before the first hello!")
+        val prefix = Prefix.getPrefix(event)
+        for(command in Commands.values()) {
+            embed.addField("${prefix}${command.toLowerName()}", command.description, false)
+        }
+        Helper.queueEmbed(event, embed)
     }
 }
