822b71e8433818ddd94ab2196887f6c17b9f1bdc
Rotinx <39930952+Rotinx@users.noreply.github.com>
2020-11-11 10:19:39 +1300

    Fixed timeout connection when returning inside of the query codeblock

--- src/main/kotlin/utils/Inlines.kt
+++ src/main/kotlin/utils/Inlines.kt
@@ -3,12 +3,12 @@ package utils
 import database.Database
 import java.sql.Connection
 
-/**
- *
- */
 inline fun query(block: (connection: Connection) -> Unit, close: Boolean = true, commit: Boolean = false) {
     val connection = Database.get()
-    block(connection)
-    if (commit) connection.commit()
-    if (close) connection.close()
+    try {
+        return block(connection)
+    } finally {
+        if (commit) connection.commit()
+        if (close) connection.close()
+    }
 }
