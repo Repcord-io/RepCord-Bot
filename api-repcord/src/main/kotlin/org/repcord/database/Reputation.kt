79819ad67edd93f15a99ee686507da84e57ba984
Rotinx <39930952+Rotinx@users.noreply.github.com>
2020-11-16 20:10:13 +1300

    Added reputations endpoint

---
+++ api-repcord/src/main/kotlin/org/repcord/database/Reputation.kt
@@ -0,0 +1,41 @@
+package org.repcord.database
+
+import utils.query
+import java.sql.PreparedStatement
+import java.sql.Timestamp
+
+object Reputation {
+    fun getUsersRep(id: String?): MutableList<ReputationModal> {
+        // Get users rep verbose :ReputationModal
+        val reputations = mutableListOf<ReputationModal>()
+        query({
+            val st: PreparedStatement = it.prepareStatement("SELECT date_given, user_cache.username, user_cache.avatar, rep, comment FROM user_reputation INNER JOIN user_cache ON user_reputation.giver = user_cache.userid WHERE user_reputation.user = ?")
+            st.setString(1, id)
+            val rs = st.executeQuery()
+            it.commit()
+            while (rs.next()) {
+                reputations.add(
+                    ReputationModal(
+                        rs.getTimestamp(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getString(5)
+                    )
+                )
+            }
+
+            return reputations
+        })
+        return reputations
+    }
+}
+
+data class ReputationModal(
+    val created: Timestamp,
+    val user: String, // User who gave the reputation.
+    val avatar: String?,
+    val score: Int, // Score/power added to rep.
+    val comment: String
+){
+}
--- api-repcord/src/main/kotlin/org/repcord/routes/User.kt
+++ api-repcord/src/main/kotlin/org/repcord/routes/User.kt
@@ -6,6 +6,7 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.repcord.database.ReputationModal
 import org.repcord.database.User
 import org.repcord.database.UserModal
 
@@ -15,13 +16,20 @@ fun Route.userRoutes(){
             val id: String? = call.parameters["id"]
 
             val user: UserModal = User.doesExist(id) ?: return@get call.respond(HttpStatusCode.NotFound, "Doesn't exist.");
-
             user.description = database.impl.User.getDescription(user.userid)
             user.donator = Donator.isOverall(user.userid)
             user.positiveTotal = Reputation.getTotalPositiveReputationsReceived(user.userid)
             user.negativeTotal = Reputation.getTotalNegativeReputationsReceived(user.userid)
             user.totalReps = Reputation.getTotalReceivedReputations(user.userid)
             return@get call.respond(user)
+        }
+    }
+
+    route("/user/{id}/reputations") {
+        get() {
+            val id: String? = call.parameters["id"]
+            val reputations : MutableList<ReputationModal> = org.repcord.database.Reputation.getUsersRep(id)
+            call.respond(reputations)
         }
     }
 }
