8bafcd9d7a0332bbf67d7a4e2a356632efb1b730
Rotinx <39930952+Rotinx@users.noreply.github.com>
2020-11-16 18:15:25 +1300

    Refactored api.kt structure.

---
+++ api-repcord/src/main/kotlin/org/repcord/Api.kt
@@ -0,0 +1,48 @@
+package org.repcord
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.repcord.routes.userRoutes
+
+fun main() {
+    Bot.main(null)
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        anyHost() // @TODO: Add domain in production.
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        userRoutes()
+    }
+}
+
